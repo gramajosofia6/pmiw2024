5d19f969ff43a020373a66ad4e40a321572b6bdb
gramajo sofia <167039548+gramajosofia6@users.noreply.github.com>
2025-10-08 20:35:52 -0300

    Update botones.js

--- tpfinalparte1/botones.js
+++ tpfinalparte1/botones.js
@@ -1,20 +1,7 @@
-//function botonSiguiente (){ 
-
-//}
-
-//function botonDecision(){
-
-//}
-
-//function botonReiniciar(){
-
-//}
-
-/**
- * Verifica si las coordenadas actuales del ratón están dentro del área de un botón.
- * @param {object} boton - Objeto con propiedades x, y, w, h del botón.
- * @returns {boolean} True si el ratón está sobre el botón, False en caso contrario.
- */
+// para verificar si las coordenadas actuales del moouse están dentro del área de un botón.
+  //@param {object} boton - Objeto con propiedades x, y, w, h del botón.
+  //@returns {boolean} True si el mouse está sobre el botón, False si no está
+ 
 function mouseEnBoton(boton) {
   return mouseX > boton.x && 
          mouseX < boton.x + boton.w && 
@@ -22,14 +9,15 @@ function mouseEnBoton(boton) {
          mouseY < boton.y + boton.h;
 }
 
-/**
- * Dibuja un botón en la pantalla.
- * @param {object} boton - Objeto con propiedades x, y, w, h y texto.
- */
+
+ // Dibuja un botón en la pantalla.
+ //@param {object} boton - Objeto con propiedades x, y, w, h y texto.
+ 
 function dibujarBoton(boton) {
   // Dibuja el rectángulo del botón
-  fill(0, 150); // Fondo negro semi-transparente
-  stroke(255); // Borde blanco
+  fill(35,208,229); // Fondo negro semi-transparente
+  stroke (250,200,18);
+  strokeWeight(4); 
   rect(boton.x, boton.y, boton.w, boton.h, 10); // rect con esquinas redondeadas
   
   // Dibuja el texto
@@ -38,4 +26,22 @@ function dibujarBoton(boton) {
   textSize(24);
   textAlign(CENTER, CENTER);
   text(boton.texto, boton.x + boton.w/2, boton.y + boton.h/2);
+}
+
+ function dibujarBotonesDeDecision(textoIzquierda, textoDerecha) {
+    let w = 250;
+    let h = 60;
+    let margen = 20;
+    let y = height - h - margen; // Posición vertical fija cerca del fondo
+
+    // 1. Botón 1 (Izquierda)
+    let x1 = margen; 
+    let boton1 = { x: x1, y: y, w: w, h: h, texto: textoIzquierda };
+    dibujarBoton(boton1);
+    
+    // 2. Botón 2 (Derecha)
+    let x2 = width - w - margen; 
+    let boton2 = { x: x2, y: y, w: w, h: h, texto: textoDerecha };
+    dibujarBoton(boton2);
+    
 }
