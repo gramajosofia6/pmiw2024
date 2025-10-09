5b89973e1f952713c8fa560ba17c2579edaece10
gramajo sofia <167039548+gramajosofia6@users.noreply.github.com>
2025-10-09 15:37:32 -0300

    Update creditos.js

--- tpfinalparte1/creditos.js
+++ tpfinalparte1/creditos.js
@@ -1,8 +1,11 @@
 function creditos() {
-  // -------- PANTALLA DE CREDITOS--------
-  image(imagenes[19], 0, 0, width, height);
+  // -------- PANTALLA DE CREDITOS--------// esta pantalla todavia no esta andando
+  image(imagenes[20],0,0, width, height);
   fill(255);
-  textSize(24);
+  textSize(16);
   textAlign(CENTER, CENTER);
-  text("Créditos:\n Godoy Lourdes y Sofia Gramajo \n    PMIW \n    ©2025", width/2, height/2);
+  text(" Creadoras:                   \n     \n Godoy Lourdes y Sofia Gramajo \n  \n                                 PMIW - Comision 5 \n                     \n                      2025 ©", width / 2, height / 3);
+  
 }
+
+   //ACA FALTARIA AGREGAR EL BOTON DE REINICIO PARA QUE TE LLEVE A LA PORTADA DEVUELTA  
